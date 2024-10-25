f66fe4cb8764ca612b67fc8d850fb64c2dfb9d8c
Gabriel Ribeiro <105074547+gabriersdev@users.noreply.github.com>
2024-10-25 15:46:42 -0300

    feat: organizando estrutura para desenvolver o projeto

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,10 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+// Import Bootstrap CSS e JS
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min'
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
