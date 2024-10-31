9323b2dcd5729c40818f3c503c52355e4ab01e10
Gabriel Ribeiro <105074547+gabriersdev@users.noreply.github.com>
2024-10-31 17:07:47 -0300

    feat: estilizando

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -2,15 +2,17 @@ import React from "react";
 import { Container } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
+import "./footer.css";
+
 const Footer = () => {
   return (
-    <footer className="d-flex flex-column gap-5">
-      <Container>
-        <p>(C) {new Date().getFullYear() || '2024'} Mobilidade</p>
-        <ul className="d-flex flex-column g-3 m-0 p-0">
-          <Link to={"/development"} className="">Desenvolvimento</Link>
-          <Link to="/terms-of-service" className="">Termos de serviços</Link>
-          <Link to="/privacy" className="">Privacidade</Link>
+    <footer className="footer border-top">
+      <Container className="d-flex flex-column gap-2rem">
+        <p className="mb-0" style={{ color: '#4C4C4C' }}>(C) {new Date().getFullYear() || '2024'} Mobilidade</p>
+        <ul className="d-flex flex-column g-3 m-0 p-0 footer-link-list">
+          <Link to={"/development"} className="footer-link-list-item">Desenvolvimento</Link>
+          <Link to="/terms-of-service" className="footer-link-list-item">Termos de serviços</Link>
+          <Link to="/privacy" className="footer-link-list-item">Privacidade</Link>
         </ul>
       </Container>
     </footer>
