090047bc5dd462bc9b88f6787a6c098bf9afa0b6
Gabriel Ribeiro <105074547+gabriersdev@users.noreply.github.com>
2024-11-25 12:26:41 -0300

    feat: iniciada implementação da funcionalidade de pesquisa

--- src/components/formSearch/FormSearch.jsx
+++ src/components/formSearch/FormSearch.jsx
@@ -1,6 +1,6 @@
 import Title from "../title/Title";
 import PropTypes from "prop-types";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { Form, FormGroup, Button } from "react-bootstrap";
 
 import './formSearch.css';
@@ -12,13 +12,29 @@ const FormSearch = ({ formTitle, inputPlaceholder }) => {
     count++;
   }, [count]);
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // Lógica de pesquisa
+    // Pode pesquisar por um nome de bairro, cidade, linha, estação ou rua
+    // No BD procurar linhas que possuem parte do nome igual ao valor de search e
+    // Linhas que possuem pontos de paradas que possuem parte do nome igual ao valor de search
+    // - O nome de cidade pode retornar as linhas que abrangem a cidade
+    // Sanitizar o valor de search para evitar SQL Injection
+
+    console.log('Form submitted');
+    console.log('Search:', search);
+  }
+
+  const [search, setSearch] = useState('');
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <FormGroup>
         <Form.Label htmlFor={`input-search-${count}`}>
           <Title title={formTitle} color="#212529" />
         </Form.Label>
-        <Form.Control type="search" id={`input-search-${count}`} placeholder={inputPlaceholder} className="w-100 fs-5" />
+        <Form.Control type="search" id={`input-search-${count}`} placeholder={inputPlaceholder} className="w-100 fs-5" value={search} onChange={(e) => setSearch(e.target.value)} required={true} />
         <Button variant="primary" style={{ display: 'none' }} type="submit" aria-hidden="true">Search</Button>
       </FormGroup>
     </form>
