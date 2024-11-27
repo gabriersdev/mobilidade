dc1832f369095a66117ca4cebf04dfc36fd538f3
Gabriel Ribeiro <105074547+gabriersdev@users.noreply.github.com>
2024-11-27 12:07:01 -0300

    dev: script de rascunho adicionado

--- database/draft.sql
+++ database/draft.sql
@@ -3,3 +3,6 @@ SELECT * FROM `lines` WHERE UPPER(line_name) LIKE UPPER('%%') OR UPPER(departure
 
 -- Pesquisa de linhas no banco de dados
 SELECT l.`line_name`, dp.line_id FROM departure_points AS dp LEFT JOIN `lines` AS l ON dp.line_id = l.line_id WHERE address LIKE '%XXXX%' OR dp.observations LIKE '%XXXX%';
+
+-- Pesquisas os dias que tem horários registrados de uma linha
+SELECT DISTINCT dt.`day` AS `operation_days` FROM departure_times AS dt, `lines` AS L WHERE dt.line_id = l.line_id AND l.line_id = 4
