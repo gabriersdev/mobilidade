9ab185e8d84f295fd0136c0bcf4322cb5b7cade4
Gabriel Ribeiro <105074547+gabriersdev@users.noreply.github.com>
2024-11-29 11:36:24 -0300

    feat: improviso tecnico - atraso na alteração do estado quando a pesquisa for válida

--- src/components/formSearch/FormSearch.jsx
+++ src/components/formSearch/FormSearch.jsx
@@ -6,28 +6,29 @@ import { Form, FormGroup, Button } from "react-bootstrap";
 import './formSearch.css';
 
 // TODO - Testar a lógica implementada para atualizar os dados da página
-const FormSearch = ({ formTitle, inputPlaceholder, fnSetIsValidSearch }) => {
+const FormSearch = ({ formTitle, inputPlaceholder, fnSetIsValidSearch, fnSetTermSearch }) => {
   const [search, setSearch] = useState('');
   const [feedback, setFeedback] = useState('');
 
-  useEffect(() => {
-    console.log(fnSetIsValidSearch)
-  }, []);
-
   const handleSubmit = (e) => {
+    fnSetIsValidSearch(false);
+    fnSetTermSearch(null);
+
     e.preventDefault();
 
-    if (search.trim().length === 0) {
-      setFeedback('O campo de pesquisa não pode estar vazio.');
-      fnSetIsValidSearch(false);
-      return;
-    } else if (search.trim().length < 3) {
-      setFeedback('O termo para pesquisa deve conter pelo menos 3 caracteres.');
-      fnSetIsValidSearch(false);
-      return;
-    }
-
-    fnSetIsValidSearch(search);
+    setTimeout(() => {
+      if (search.trim().length === 0) {
+        setFeedback('O campo de pesquisa não pode estar vazio.');
+        fnSetIsValidSearch(false);
+      } else if (search.trim().length < 3) {
+        setFeedback('O termo para pesquisa deve conter pelo menos 3 caracteres.');
+        fnSetIsValidSearch(false);
+      } else {
+        setFeedback('');
+        fnSetIsValidSearch(true);
+        fnSetTermSearch(search);
+      }
+    }, 0)
   }
 
   return (
@@ -36,7 +37,7 @@ const FormSearch = ({ formTitle, inputPlaceholder, fnSetIsValidSearch }) => {
         <Form.Label htmlFor={`input-search`}>
           <Title title={formTitle} color="#212529" />
         </Form.Label>
-        <Form.Control type="search" id={`input-search`} placeholder={inputPlaceholder} className="w-100 fs-5" value={search} onChange={(e) => setSearch(e.target.value)} required={false} />
+        <Form.Control type="search" id={`input-search`} placeholder={inputPlaceholder} className="w-100 fs-5" value={search} onChange={(e) => setSearch(e.target.value)} />
         <Button variant="primary" style={{ display: 'none' }} type="submit" aria-hidden="true">Search</Button>
       </FormGroup>
       <span className={"d-block mt-2 text-danger"}>{feedback}</span>
@@ -47,7 +48,8 @@ const FormSearch = ({ formTitle, inputPlaceholder, fnSetIsValidSearch }) => {
 FormSearch.propTypes = {
   formTitle: PropTypes.string.isRequired,
   inputPlaceholder: PropTypes.string.isRequired,
-  fnSetIsValidSearch: PropTypes.func.isRequired
+  fnSetIsValidSearch: PropTypes.func.isRequired,
+  fnSetTermSearch: PropTypes.func.isRequired
 }
 
 export default FormSearch;
