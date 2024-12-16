76a8f0c84ffa5af33354aaa1f5d8ddb61ffe0c60
Gabriel Ribeiro <105074547+gabriersdev@users.noreply.github.com>
2024-12-16 11:25:12 -0300

    fix: escape quando title não for informado

--- src/assets/util.js
+++ src/assets/util.js
@@ -105,6 +105,8 @@ export default class Util {
   }
 
   static convertToSafeText(title) {
+    if (!title) return '';
+
     let sanitize = title.normalize("NFD").replace(/[\u0300-\u036f]/g, "").replace(/[\u0100-\u1EFF]/g, "")
     sanitize = sanitize.toLowerCase().replace(/\s/g, "-")
     sanitize = sanitize.replace(/[,.]/g, '')
