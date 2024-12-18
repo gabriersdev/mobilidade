010eb8fec2b1e0038343ef6680596ae83f9a7fb9
Gabriel Ribeiro <105074547+gabriersdev@users.noreply.github.com>
2024-12-18 15:27:43 -0300

    feat: alterando estrutura do server.js

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 import express from 'express';
 import mysql from 'mysql2/promise';
 import cors from 'cors';
-import { stat } from 'node:fs/promises';
+import {stat} from 'node:fs/promises';
+
+import configs from './config.js'
 
 const app = express();
 const port = 3001;
@@ -9,12 +11,24 @@ const port = 3001;
 app.use(cors());
 app.use(express.json());
 
-const dbConfig = {
-  host: 'localhost',
-  user: 'root',
-  password: 'root',
-  database: 'bus_system',
-};
+const mode = 1
+let dbConfig = {};
+
+if (mode === 1) {
+  dbConfig = {
+    host: configs.host,
+    user: configs.user,
+    password: configs.password,
+    database: configs.database,
+  };
+} else if (mode === 0) {
+  dbConfig = {
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'bus_system',
+  };
+}
 
 const pool = mysql.createPool(dbConfig);
 const originRequest = 'http://localhost:5173';
@@ -38,7 +52,7 @@ app.get('/api/lines/count', async (req, res) => {
     connection.release();
     res.json(rows);
   } catch (error) {
-    res.status(500).json({ error: `Erro ao consultar o banco de dados. ${error.message}` });
+    res.status(500).json({error: `Erro ao consultar o banco de dados. ${error.message}`});
   }
 })
 
@@ -55,7 +69,7 @@ app.get('/api/lines/', async (req, res) => {
 
     res.json(rows);
   } catch (error) {
-    res.status(500).json({ error: `Erro ao consultar o banco de dados. ${error.message}` });
+    res.status(500).json({error: `Erro ao consultar o banco de dados. ${error.message}`});
   }
 })
 
@@ -63,14 +77,14 @@ app.get('/api/lines/', async (req, res) => {
 app.post('/api/lines/', async (req, res) => {
   try {
     res = setHeaderHTTP(res, 'POST');
-    const { id } = req.body
+    const {id} = req.body
 
     const connection = await pool.getConnection();
     const [rows] = await connection.execute('SELECT line_id, line_number, line_name, departure_location, destination_location, fare, `lines`.company_id, `companies`.company_name, system_id, `mode`, is_public, has_integration, observations, `type`, scope FROM `lines`, `companies` WHERE `lines`.company_id = `companies`.company_id AND `lines`.active = 1 AND line_id = ?;', [id]);
     connection.release();
     res.json(rows);
   } catch (error) {
-    res.status(500).json({ error: `Erro ao consultar o banco de dados. ${error.message}` });
+    res.status(500).json({error: `Erro ao consultar o banco de dados. ${error.message}`});
   }
 })
 
@@ -84,7 +98,7 @@ app.get('/api/lines/main', async (req, res) => {
     connection.release();
     res.json(rows);
   } catch (error) {
-    res.status(500).json({ error: `Erro ao consultar o banco de dados. ${error.message}` });
+    res.status(500).json({error: `Erro ao consultar o banco de dados. ${error.message}`});
   }
 })
 
@@ -94,14 +108,14 @@ app.post('/api/departure_times/filter_direction', async (req, res) => {
     setHeaderHTTP(res, 'POST');
 
     // Recebe via POST o id da linha e o sentido da viagem
-    const { line_id, direction } = req.body;
+    const {line_id, direction} = req.body;
 
     const connection = await pool.getConnection();
     const [rows] = await connection.execute('SELECT schedule_id, `day`, departure_time, (SELECT COUNT(*) FROM departure_times_observations WHERE departure_time_id = schedule_id) AS has_observation FROM departure_times WHERE active = 1 AND line_id = ? AND direction = ? ORDER BY day, departure_time;', [line_id, direction]);
     connection.release();
     res.json(rows);
   } catch (error) {
-    res.status(500).json({ error: `Erro ao consultar o banco de dados. ${error.message}` });
+    res.status(500).json({error: `Erro ao consultar o banco de dados. ${error.message}`});
   }
 })
 
@@ -110,14 +124,14 @@ app.post('/api/departure_times/', async (req, res) => {
     setHeaderHTTP(res, 'POST');
 
     // Recebe via POST o id da linha e o sentido da viagem
-    const { line_id } = req.body;
+    const {line_id} = req.body;
 
     const connection = await pool.getConnection();
     const [rows] = await connection.execute('SELECT schedule_id, `day`, departure_time, (SELECT COUNT(*) FROM departure_times_observations WHERE departure_time_id = schedule_id) AS has_observation, direction FROM departure_times WHERE active = 1 AND line_id = ? ORDER BY day, departure_time;', [line_id]);
     connection.release();
     res.json(rows);
   } catch (error) {
-    res.status(500).json({ error: `Erro ao consultar o banco de dados. ${error.message}` });
+    res.status(500).json({error: `Erro ao consultar o banco de dados. ${error.message}`});
   }
 })
 
@@ -127,14 +141,14 @@ app.post('/api/departure_times_observations/', async (req, res) => {
     res = setHeaderHTTP(res, 'POST');
 
     // Recebe id da linha
-    const { line_id } = req.body;
+    const {line_id} = req.body;
 
     const connection = await pool.getConnection();
     const [rows] = await connection.execute('SELECT l.line_name, dt.direction, dp.departure_time_id, dp.observation_id, o.observation_name, o.observation_abrev FROM departure_times_observations AS dp LEFT JOIN departure_times AS dt ON dp.departure_time_id = dt.schedule_id LEFT JOIN `lines` AS l ON l.line_id = dt.line_id LEFT JOIN observations AS o ON dp.observation_id = o.observation_id WHERE l.line_id = ? AND l.active = 1 AND dt.active = 1 ORDER BY l.line_id;', [line_id]);
     connection.release();
     res.json(rows);
   } catch (error) {
-    res.status(500).json({ error: `Erro ao consultar o banco de dados. ${error.message}` });
+    res.status(500).json({error: `Erro ao consultar o banco de dados. ${error.message}`});
   }
 })
 
@@ -142,14 +156,14 @@ app.post('/api/departure_times_observations/', async (req, res) => {
 app.post('/api/departure_points/', async (req, res) => {
   try {
     res = setHeaderHTTP(res, 'POST');
-    const { line_id } = req.body;
+    const {line_id} = req.body;
 
     const connection = await pool.getConnection();
     const [rows] = await connection.execute('SELECT departure_point_id, point_name, direction, address, observations FROM departure_points WHERE line_id = ? ORDER BY order_departure_point, direction;', [line_id]);
     connection.release();
     res.json(rows);
   } catch (error) {
-    res.status(500).json({ error: `Erro ao consultar o banco de dados. ${error.message}` });
+    res.status(500).json({error: `Erro ao consultar o banco de dados. ${error.message}`});
   }
 })
 
@@ -157,14 +171,14 @@ app.post('/api/departure_points/', async (req, res) => {
 app.post('/api/recharge_points/', async (req, res) => {
   try {
     res = setHeaderHTTP(res, 'POST');
-    const { id_company } = req.body;
+    const {id_company} = req.body;
 
     const connection = await pool.getConnection();
     const [rows] = await connection.execute('SELECT point_name, `address`, `observations`, `link_google_maps` FROM recharge_points WHERE company_id = ? ORDER BY point_name, `address`;', [id_company]);
     connection.release();
     res.json(rows);
   } catch (error) {
-    res.status(500).json({ error: `Erro ao consultar o banco de dados. ${error.message}` });
+    res.status(500).json({error: `Erro ao consultar o banco de dados. ${error.message}`});
   }
 })
 
@@ -173,14 +187,14 @@ app.post('/api/logs/', async (req, res) => {
   try {
     setHeaderHTTP(res, 'POST');
 
-    const { event_type, event_details, os, browser, ip_address, user_agent } = req.body;
+    const {event_type, event_details, os, browser, ip_address, user_agent} = req.body;
 
     const connection = await pool.getConnection();
     await connection.execute('INSERT INTO bus_system.logs (origin, event_type, event_details, os, browser, ip_address, user_agent) VALUES (`application-node-web`, ?, ?, ?, ?, ?, ?);', [event_type, event_details, os, browser, ip_address, user_agent]);
     connection.release();
-    res.json({ success: 'Log inserido com sucesso' });
+    res.json({success: 'Log inserido com sucesso'});
   } catch (error) {
-    res.status(500).json({ error: `Erro ao inserir dados no banco de dados. ${error.message}` });
+    res.status(500).json({error: `Erro ao inserir dados no banco de dados. ${error.message}`});
   }
 })
 
@@ -189,7 +203,7 @@ app.post('/api/lines/search', async (req, res) => {
   try {
     setHeaderHTTP(res, 'POST');
 
-    const { search } = req.body;
+    const {search} = req.body;
 
     const connection = await pool.getConnection();
 
@@ -203,7 +217,7 @@ app.post('/api/lines/search', async (req, res) => {
     connection.release();
     res.json(rows);
   } catch (error) {
-    res.status(500).json({ error: `Erro ao consultar o banco de dados. ${error.message}` });
+    res.status(500).json({error: `Erro ao consultar o banco de dados. ${error.message}`});
   }
 })
 
@@ -216,7 +230,7 @@ app.get('/api/file_date/:filename', async (req, res) => {
     const mtime = new Date(stats.mtimeMs);
 
     // Envia o timestamp como JSON
-    res.json({ mtime: stats.mtimeMs });
+    res.json({mtime: stats.mtimeMs});
 
     // Formatando a data (opcional)
     const options = {
@@ -229,10 +243,10 @@ app.get('/api/file_date/:filename', async (req, res) => {
     };
 
     const formattedDate = mtime.toLocaleDateString(undefined, options);
-    res.json({ date: formattedDate });
+    res.json({date: formattedDate});
   } catch (error) {
     console.error("Erro ao obter informações do arquivo:", error);
-    res.status(500).json({ error: 'Erro ao obter a data do arquivo' });
+    res.status(500).json({error: 'Erro ao obter a data do arquivo'});
   }
 });
 
@@ -241,7 +255,7 @@ app.post('/api/departure_points/insert/', async (req, res) => {
   try {
     setHeaderHTTP(res, 'POST');
 
-    const { line_id, direction, order_departure_point, point_name, address, city_id, state_id, observations } = req.body;
+    const {line_id, direction, order_departure_point, point_name, address, city_id, state_id, observations} = req.body;
 
     const connection = await pool.getConnection();
 
@@ -250,7 +264,7 @@ app.post('/api/departure_points/insert/', async (req, res) => {
     connection.release();
     res.json(rows);
   } catch (error) {
-    res.status(500).json({ error: `Erro ao inserir dados no banco de dados. ${error.message}` });
+    res.status(500).json({error: `Erro ao inserir dados no banco de dados. ${error.message}`});
   }
 })
 
