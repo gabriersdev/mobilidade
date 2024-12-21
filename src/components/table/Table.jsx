2c132be73c44107bc6bb8e822f1e060bec2f247e
Gabriel Ribeiro <105074547+gabriersdev@users.noreply.github.com>
2024-12-21 13:18:58 -0300

    feat: atualizando componentes

--- src/components/table/Table.jsx
+++ src/components/table/Table.jsx
@@ -1,40 +1,51 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-import { Table as BootstrapTable } from 'react-bootstrap';
+import {Badge, Table as BootstrapTable} from 'react-bootstrap';
+import data from "../../data.js";
 
-const listItems = (data) => {
-  const newData = [...data];
-  const itemsPerLine = 12;
+const listItems = (listData, observations) => {
+  const newData = [...listData];
+  const itemsPerLine = 10;
   const rows = [];
 
-  console.log('INIT ##')
   while (newData.length > 0) {
-    rows.push(
-      <tr key={rows.length}>
-        {newData.splice(0, itemsPerLine).map((item, i) => <td key={`${rows.length}-${i}`}>{JSON.stringify(item)}</td>)}
-      </tr>
-    );
+    rows.push(<tr key={rows.length}>
+      {
+        newData.splice(0, itemsPerLine).map((item, i) => {
+          const bootstrapBGColors = data.bootstrap.bg.colors;
+          return (
+            <td key={`${rows.length}-${i}`}>
+              <div className={"d-flex align-items-center"}>
+                {item.departure_time}
+                {/*TODO - Usar tooltip*/}
+                {item.observation ? <Badge
+                  bg={bootstrapBGColors.at(observations.findIndex((o) => o.abrev === item.observation[0])) || 'primary'}
+                  className={"ms-1"} title={item.observation[1]}>{item.observation[0]}</Badge> : ''}
+              </div>
+            </td>
+          )
+        })
+      }
+    </tr>);
   }
 
   return rows;
 }
 
-const Table = ({ content }) => {
-  console.log(content)
-  return (
-    <BootstrapTable responsive className="table-line-content">
-      <tbody>
-        {listItems(content.data)}
-      </tbody>
-    </BootstrapTable>
-  )
+const Table = ({content, observations}) => {
+  return (<BootstrapTable responsive className="table-line-content mb-0 pb-0">
+    <tbody>
+    {listItems(content.data, observations)}
+    </tbody>
+  </BootstrapTable>)
 }
 
 Table.propTypes = {
   content: PropTypes.shape({
-    data: PropTypes.array.isRequired
-  }).isRequired
+    data: PropTypes.array.isRequired,
+  }).isRequired,
+  observations: PropTypes.array
 }
 
 export default Table;
