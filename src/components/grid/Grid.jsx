18bebe3c4a69625e9ba88813fe954ae057188e04
Gabriel Ribeiro <105074547+gabriersdev@users.noreply.github.com>
2024-12-21 13:28:52 -0300

    fix: corrindo nome do arquivo

--- src/components/grid/Grid.jsx
+++ src/components/grid/Grid.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-import "./Grid.css";
+import "./grid.css";
 
 const Grid = ({ classes, children }) => {
   return <div className={classes ? `grid ${classes}` : "grid"}>{children}</div>;
