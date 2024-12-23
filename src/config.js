d9dd4c2b5eefe73da1a9dafbaffc4558bd20c68d
Gabriel Ribeiro <105074547+gabriersdev@users.noreply.github.com>
2024-12-23 15:43:34 -0300

    feat: alteracao de host conforme hostname da origem

--- src/config.js
+++ src/config.js
@@ -1,4 +1,13 @@
-export default {
-  // host: 'http://localhost:3001',
-  host: 'https://mobilidade-server-production.up.railway.app'
+let config;
+
+if (window.location.hostname === 'localhost') {
+  config = {
+    host: 'http://localhost:3001'
+  };
+} else {
+  config = {
+    host: 'https://mobilidade-server-production.up.railway.app'
+  };
 }
+
+export default config;
