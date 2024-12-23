35d99043b872f610f1d5da4b78ade520ae3b589f
Gabriel Ribeiro <105074547+gabriersdev@users.noreply.github.com>
2024-12-23 15:54:51 -0300

    feat: chuncks para rollup adicionados

--- vite.config.js
+++ vite.config.js
@@ -7,5 +7,40 @@ export default defineConfig({
   plugins: [react()],
   server: {
     historyApiFallback: true
-  }
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules/react')) {
+            return 'vendor-react';
+          }
+          if (id.includes('node_modules/react-dom')) {
+            return 'vendor-react-dom';
+          }
+          if (id.includes('node_modules/bootstrap')) {
+            return 'vendor-bootstrap';
+          }
+          if (id.includes('node_modules/react-bootstrap')) {
+            return 'vendor-react-bootstrap';
+          }
+          if (id.includes('node_modules/bootstrap-icons')) {
+            return 'vendor-bootstrap-icons';
+          }
+          if (id.includes('node_modules/moment')) {
+            return 'vendor-moment';
+          }
+          if (id.includes('/src/components/')) {
+            return 'components'; // Agrupa componentes em um chunk
+          }
+          if (id.includes('/src/pages/')) {
+            return 'pages'; // Agrupa utilitários em um chunk
+          }
+          if (id.includes('/src/assets/')) {
+            return 'assets'; // Agrupa os dados em um chunk
+          }
+        },
+      },
+    },
+  },
 })
