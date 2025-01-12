756d3144411f73715b6ba77af2fedc9e9939ef0d
Gabriel Ribeiro <105074547+gabriersdev@users.noreply.github.com>
2025-01-12 10:16:24 -0300

    feat: alterando textos de alerta

--- src/components/weather/Weather.jsx
+++ src/components/weather/Weather.jsx
@@ -25,14 +25,14 @@ const Weather = () => {
   }, []);
 
   if (status === 'loading') {
-    return <></>; // Ou um componente de loading mais visual
+    return <></>;
   }
 
   if (status === 'error') {
-    return <></>; // Ou um componente de erro
+    return <></>;
   }
 
-  if (!weatherData) { // Segurança adicional
+  if (!weatherData) {
     return <></>;
   }
 
@@ -41,18 +41,17 @@ const Weather = () => {
   try {
     if (weatherData.current.precip_mm > 0) {
       if (weatherData.current.precip_mm <= 2) {
-        alertText = 'Previsão de chuva leve. Normalmente não causa transtornos ao trânsito ou atraso nos horários de partida.'
+        alertText = 'Previsão de chuva leve para a região. Normalmente não causa transtornos ao trânsito ou atraso nos horários de partida.'
       } else if (weatherData.current.precip_mm <= 10) {
-        alertText = 'Previsão de chuva moderada. Pode causar atrasos nos horários de partida.'
+        alertText = 'Previsão de chuva moderada para a região.. Pode causar atrasos nos horários de partida.'
       } else if (weatherData.current.precip_mm <= 30) {
-        alertText = 'Fique ligado! Previsão de chuva forte. Pode causar atrasos nos horários de partida e transtornos ao trânsito.'
+        alertText = 'Fique ligado! Previsão de chuva forte para a região.. Pode causar atrasos nos horários de partida e transtornos ao trânsito.'
       } else {
-        alertText = 'Atenção! Previsão de chuva muito forte/torrencial. O que deve causar atrasos nos horários de partida e transtornos ao trânsito. Se possível, evite sair de casa e fique em um local fechado e seguro.'
+        alertText = 'Atenção! Previsão de chuva muito forte/torrencial para a região. O que deve causar atrasos nos horários de partida e transtornos ao trânsito. Se possível, evite sair de casa e fique em um local fechado e seguro.'
       }
     }
   } catch (e) {
     console.error('Erro ao processar dados:', e);
-    // Manter o texto padrão caso haja erro no processamento dos dados
   }
 
   if (!alertText) return <></>
