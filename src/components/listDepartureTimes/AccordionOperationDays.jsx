80032e99f1e4293c3c2cda58e185eb7ec64d8679
Gabriel Ribeiro <105074547+gabriersdev@users.noreply.github.com>
2025-01-25 10:54:06 -0300

    feat: lógica para exibir aviso sobre clima alterada; accordion de horario que abre alterado

--- src/components/listDepartureTimes/AccordionOperationDays.jsx
+++ src/components/listDepartureTimes/AccordionOperationDays.jsx
@@ -1,4 +1,4 @@
-import {useContext} from "react";
+import {useContext, useState} from "react";
 
 import {Theme} from "../themeContext/ThemeContext";
 import AccordionItem from "../accordion/AccordionItem";
@@ -9,30 +9,48 @@ import Accordion from "../accordion/Accordion";
 
 const AccordionOperationDays = () => {
   const {departureTimes, uniqueDaysForDirection, index, direction, directionName, observations} = useContext(Theme)
+  let defaultEventKey = ['0']
+
+  const currDayName = () => {
+    switch (new Date().getDay()) {
+      case 0:
+        return 'domingos';
+      case 1:
+      case 2:
+      case 3:
+      case 4:
+      case 5:
+        return 'dias úteis';
+      case 6:
+        return 'sábados';
+    }
+  }
+
+  const content = uniqueDaysForDirection[index].map((day, j) => {
+    if (Util.convertNumberToDay(day).toLowerCase().includes(currDayName().toLowerCase())) defaultEventKey = [j.toString()];
+    return (
+      <AccordionItem title={Util.convertNumberToDay(day)} eventKey={j.toString()} key={j}>
+        <Table
+          content={{
+            data: departureTimes.filter((item) => item.day === day && item.direction === direction).map((item) => {
+              return {
+                departureTime: Util.formatTime(`2020-01-01 ${item.departure_time}`, 'HH:mm'),
+                observations: item.observations ? item.observations : null
+              }
+            }),
+            directionName: directionName,
+            dayName: Util.convertNumberToDay(day)
+          }}
+          observations={observations}
+        />
+        {observations.length ? <Legend items={observations}/> : ""}
+      </AccordionItem>
+    )
+  })
 
   return (
-    <Accordion defaultEventKey={['0']}>
-      {uniqueDaysForDirection[index].map((day, j) => {
-        return (
-          <AccordionItem title={Util.convertNumberToDay(day)} eventKey={j.toString()} key={j}>
-            <Table
-              content={{
-                data: departureTimes.filter((item) => item.day === day && item.direction === direction).map((item) => {
-                  return {
-                    departureTime: Util.formatTime(`2020-01-01 ${item.departure_time}`, 'HH:mm'),
-                    observations: item.observations ? item.observations : null
-                  }
-                }),
-                directionName: directionName,
-                dayName: Util.convertNumberToDay(day)
-              }}
-              observations={observations}
-            />
-
-            {observations.length ? <Legend items={observations}/> : ""}
-          </AccordionItem>
-        )
-      })}
+    <Accordion defaultEventKey={defaultEventKey}>
+      {content}
     </Accordion>
   )
 }
