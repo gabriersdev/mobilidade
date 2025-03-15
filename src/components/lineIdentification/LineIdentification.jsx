04fa79b145dab37f86ce339fd01dbb375e7b815d
Gabriel Ribeiro <105074547+gabriersdev@users.noreply.github.com>
2025-03-14 22:29:24 -0300

    feat: quantidade de partidas no BD

--- src/components/lineIdentification/LineIdentification.jsx
+++ src/components/lineIdentification/LineIdentification.jsx
@@ -4,8 +4,8 @@ import Title from "../title/Title";
 import LineInfo from "../lineInfo/LineInfo";
 import {Link} from "react-router-dom";
 
-const LineIdentification = ({ line }) => {
-  let [lineType, scope, hasIntegration, fare] = ['', '', ''];
+const LineIdentification = ({line}) => {
+  let [lineType, scope, hasIntegration, fare, countDepartureTimes] = ['', '', '', 0];
 
   switch (line.type) {
     case 1:
@@ -54,38 +54,45 @@ const LineIdentification = ({ line }) => {
     }).format(line.fare);
   }
 
+  if (line.count_departure_times) countDepartureTimes = line.count_departure_times;
+
   return (
     <div className="d-flex flex-column gap-3">
       <hgroup className="d-flex align-items-center gap-2 flex-wrap mb-0">
         <Title type="h2" classX=" fs-2 d-inline text-body-emphasis m-0 p-0">Linha {line.line_number}</Title>
         <span className="text-body-secondary">|</span>
-        <Title type="h2" classX=" fs-2 d-inline text-body-secondary m-0 p-0">{line.departure_location} -{">"} {line.destination_location}</Title>
+        <Title type="h2"
+               classX=" fs-2 d-inline text-body-secondary m-0 p-0">{line.departure_location} -{">"} {line.destination_location}</Title>
       </hgroup>
       <span className={"d-block mb-5 text-body-secondary"}>{line.line_name.replace(/\//, " -> ") || ""}</span>
       <div>
         <div className="d-flex align-items-center gap-3 flex-wrap mb-3">
-          <LineInfo label={{ ref: 'Tipo da Linha', value: lineType }}>
+          <LineInfo label={{ref: 'Tipo da Linha', value: lineType}}>
             <i className="bi bi-record-circle red"></i>
           </LineInfo>
-          <LineInfo label={{ ref: 'Grupo de atendimento', value: scope }}>
+          <LineInfo label={{ref: 'Grupo de atendimento', value: scope}}>
             <i className="bi bi-building red"></i>
           </LineInfo>
-          <LineInfo label={{ ref: 'Integração com outras Linhas ou Modais', value: hasIntegration }}>
+          <LineInfo label={{ref: 'Integração com outras Linhas ou Modais', value: hasIntegration}}>
             <i className="bi bi-train-front-fill purple"></i>
           </LineInfo>
         </div>
         <div className="d-flex align-items-center gap-3 flex-wrap">
-          <LineInfo label={{ ref: 'Tarifa', value: fare }}>
+          <LineInfo label={{ref: 'Tarifa', value: fare}}>
             <i className="bi bi-cash-coin naval-blue"></i>
           </LineInfo>
           <Link to={`/company/${line.company_id}`} className={"text-decoration-none"}>
-            <LineInfo label={{ ref: 'Companhia', value: line.company_name }}>
+            <LineInfo label={{ref: 'Companhia', value: line.company_name}}>
               <i className="bi bi-buildings green-sheets"></i>
             </LineInfo>
           </Link>
+          <LineInfo label={{ref: "Horários", value: ""}}>
+            <i className="bi bi-calendar-date"></i>
+            <span className={"ms-2"}>{countDepartureTimes.toLocaleString()} partidas</span>
+          </LineInfo>
         </div>
       </div>
-    </div >
+    </div>
   )
 }
 
@@ -101,6 +108,7 @@ LineIdentification.propTypes = {
     has_integration: PropTypes.number.isRequired,
     scope: PropTypes.number.isRequired,
     type: PropTypes.number.isRequired,
+    count_departure_times: PropTypes.number.isRequired,
   }).isRequired
 }
 
