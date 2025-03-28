b586e94139ec0b15b3b074199e46633a2c508407
Gabriel Ribeiro <105074547+gabriersdev@users.noreply.github.com>
2025-03-28 09:50:21 -0300

    feat: versao do cache atualizada

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,4 @@
-const cacheVersion = "v16"
+const cacheVersion = "v17"
 const STATIC_CACHE_NAME = `mobilidade-app-${cacheVersion}`;
 const DYNAMIC_CACHE_NAME = `dynamic-mobilidade-app-${cacheVersion}`;
 
