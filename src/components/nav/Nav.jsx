3022337fcc3e56b6ea45f0f8d4a12eead186c984
Gabriel Ribeiro <105074547+gabriersdev@users.noreply.github.com>
2025-04-03 12:04:33 -0300

    feat: aviso da reducao do quadro de horarios

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -3,18 +3,32 @@ import {Link} from "react-router-dom";
 
 import "./nav.css";
 
+const infos = [
+  {
+    title: "Linhas afetadas pela redução do quadro de horários",
+    message: (<>Redução afeta as linhas 03, 04, 05 e 07 do transporte público municipal de Sabará-MG, operadas pela
+      Vinscol. Válido a partir de quinta-feira, 03<span className={"arial"}>/</span>04</>)
+  }
+]
+
+const BarInfo = () => {
+  return infos.map(({title, message}, index) => (
+    <div className={"py-4 bg-danger-subtle"} key={index}>
+      <div className="container">
+        <h2 className={"fs-6 mb-2 fw-bold text-danger"}>
+          <i className="bi bi-exclamation-triangle-fill me-2"></i>
+          {title}
+        </h2>
+        <p className={"fw-light mb-0 text-balance"}>{message}</p>
+      </div>
+    </div>
+  ))
+}
+
 const Nav = () => {
   return (
     <>
-      <div className={"py-4 bg-danger-subtle"}>
-        <div className="container">
-          <h2 className={"fs-6 mb-2 fw-bold text-danger"}>
-            <i className="bi bi-exclamation-triangle-fill me-2"></i>
-            Linhas afetadas pela redução do quadro de horários
-          </h2>
-          <p className={"fw-light mb-0 text-balance"}>Redução afeta as linhas 01, 03, 04 e 07 do transporte público municipal de Sabará-MG, operadas pela Vinscol. Válido a partir de quinta-feira, 03<span className={"arial"}>/</span>04</p>
-        </div>
-      </div>
+      <BarInfo/>
       <Navbar expand="lg" className="bg-body-tertiary border-bottom">
         <Container className="my-1 d-flex justify-content-between align-items-center w-100 flex-wrap">
           <Navbar.Brand as={Link} to="./" className={"text-body-secondary"} style={{letterSpacing: '-0.75px'}}>
@@ -31,7 +45,7 @@ const Nav = () => {
         </Container>
       </Navbar>
     </>
-)
+  )
 }
 
 export default Nav;
