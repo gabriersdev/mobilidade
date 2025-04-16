548c0586fe7ed01fabe80275293efdd5dc6a12b6
Gabriel Ribeiro <105074547+gabriersdev@users.noreply.github.com>
2025-04-16 16:21:08 -0300

    feat: aviso de chuva atualizado; text-balance no conteudo do component alert

--- src/components/weather/Weather.jsx
+++ src/components/weather/Weather.jsx
@@ -29,13 +29,18 @@ const Weather = () => {
   if (status === 'loading' || status === 'error' || !weatherData) return <></>;
   let alertText;
   const precipMM = weatherData.current.precip_mm || 0;
+  const codition = weatherData.current.condition.text || null
+  
+  console.log(precipMM, weatherData.current);
   
   try {
-    if (precipMM > 1) {
+    if (precipMM >= 1) {
       if (precipMM <= 2) alertText = 'Previsão de chuva leve para a região. Normalmente não causa transtornos ao trânsito ou atraso nos horários de partida.'
       else if (precipMM <= 10) alertText = 'Previsão de chuva moderada para a região. Pode causar atrasos nos horários de partida.'
       else if (precipMM <= 30) alertText = 'Fique ligado! Previsão de chuva forte para a região. Pode causar atrasos nos horários de partida e transtornos ao trânsito.'
       else alertText = 'Atenção! Previsão de chuva muito forte/torrencial para a região. O que deve causar atrasos nos horários de partida e transtornos ao trânsito. Se possível, evite sair de casa e fique em um local fechado e seguro.'
+    } else if (codition.toLowerCase().includes('rain')) {
+      alertText = 'Chuva na região. Saia mais cedo e com guarda-chuvas. A chuva pode atrasar os horários de partida e causar transtornos no trânsito.'
     }
   } catch (e) {
     console.error('Erro ao processar dados:', e);
