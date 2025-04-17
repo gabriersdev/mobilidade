4b6a7d0bbf09b19ddd367de606d4cd691405cfb4
Gabriel Ribeiro <105074547+gabriersdev@users.noreply.github.com>
2025-04-16 22:28:33 -0300

    feat: animacoes

--- src/pages/company/Company.jsx
+++ src/pages/company/Company.jsx
@@ -6,6 +6,7 @@ import config from "../../config.js";
 import FeedbackError from "../../components/feedbackError/FeedbackError.jsx";
 import Title from "../../components/title/Title.jsx";
 import {Badge} from "react-bootstrap";
+import AnimatedComponents from "../../components/animatedComponent/AnimatedComponents.jsx";
 
 const RenderCompany = () => {
   const {id} = useParams();
@@ -67,40 +68,43 @@ const RenderCompany = () => {
     if (dataCompanyId) dataCompanyId.querySelector('a').textContent = `${data[0].company_name}`;
     
     return (
-      <>
+      <AnimatedComponents>
         <span className={"text-body-secondary"}>Companhia</span>
         <Title type="h2" classX=" fs-2 d-inline text-body-emphasis mt-1 p-0 d-block">
         <span className="text-balance" style={{fontSize: "inherit"}}>
           {data[0].company_name}
         </span>
         </Title>
+        
         <section className={"d-flex gap-5 mt-5 flex-column"}>
-          <div className="d-flex flex-column gap-1">
-            <span className={"text-body-tertiary"}>Contato</span>
-            <span className={""}>{data[0].contact || 'Informação não cadastrada.'}</span>
-          </div>
-          <div className="d-flex flex-column gap-1">
-            <span className="text-body-tertiary">Canal de reclamações</span>
-            <Link to={data[0].report_contact} rel={"noreferrer noopener"}>{new URL(data[0].report_contact).origin || "Informação não cadastrada."}</Link>
-          </div>
-          <div className="d-flex flex-column gap-1">
-            <span className={"text-body-tertiary"}>Observações</span>
-            <span className={""}>
+          <AnimatedComponents>
+            <div className="d-flex flex-column gap-1">
+              <span className={"text-body-tertiary"}>Contato</span>
+              <span className={""}>{data[0].contact || 'Informação não cadastrada.'}</span>
+            </div>
+            <div className="d-flex flex-column gap-1">
+              <span className="text-body-tertiary">Canal de reclamações</span>
+              <Link to={data[0].report_contact} rel={"noreferrer noopener"}>{new URL(data[0].report_contact).origin || "Informação não cadastrada."}</Link>
+            </div>
+            <div className="d-flex flex-column gap-1">
+              <span className={"text-body-tertiary"}>Observações</span>
+              <span className={""}>
               {countLines ? `${countLines} ${countLines > 0 ? "linhas" : "linha"} operadas pela companhia.` : 'Opera linhas de transporte público no estado de Minas Gerais.'}
             </span>
-          </div>
-          <div className="d-flex flex-column gap-1">
-            <span className={"text-body-tertiary"}></span>
-            <div className={"d-flex flex-wrap column-gap-2 row-gap-3"}>
-              {linesData ? linesData.map((line, index) => (
-                <Badge key={index} variant="primary" className={"rounded-5 fw-light text-decoration-none"} style={{letterSpacing: '0.5px'}} as={Link} to={`/lines/${line.line_id}`}>
-                  N.º {line.line_number}
-                </Badge>
-              )) : ""}
             </div>
-          </div>
+            <div className="d-flex flex-column gap-1">
+              <span className={"text-body-tertiary"}></span>
+              <div className={"d-flex flex-wrap column-gap-2 row-gap-3"}>
+                {linesData ? linesData.map((line, index) => (
+                  <Badge key={index} variant="primary" className={"rounded-5 fw-light text-decoration-none"} style={{letterSpacing: '0.5px'}} as={Link} to={`/lines/${line.line_id}`}>
+                    N.º {line.line_number}
+                  </Badge>
+                )) : ""}
+              </div>
+            </div>
+          </AnimatedComponents>
         </section>
-      </>
+      </AnimatedComponents>
     );
   }
 }
