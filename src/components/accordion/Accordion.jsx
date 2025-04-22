f5e7c5f28d4729435cd529d240f1a7986106b439
Gabriel Ribeiro <105074547+gabriersdev@users.noreply.github.com>
2025-04-22 09:22:24 -0300

    Update Accordion.jsx

--- src/components/accordion/Accordion.jsx
+++ src/components/accordion/Accordion.jsx
@@ -4,13 +4,6 @@ import {Accordion as BootstrapAccordion} from 'react-bootstrap';
 import {useEffect} from "react";
 
 function Accordion({defaultEventKey, title, children}) {
-  useEffect(() => {
-    // Define o titulo do accordion com text-balance
-    document.querySelectorAll('.lh-base.accordion-header button').forEach(el => {
-      el.classList.add('text-balance');
-    })
-  }, []);
-  
   return (
     <BootstrapAccordion defaultActiveKey={defaultEventKey} aria-description={title ? title.trim() : ''} alwaysOpen>
       {children}
