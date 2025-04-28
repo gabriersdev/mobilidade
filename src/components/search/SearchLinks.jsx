9c6522eba4c3d01b0e586dbc139fbe4cff5387a4
Gabriel Ribeiro <105074547+gabriersdev@users.noreply.github.com>
2025-04-28 12:13:19 -0300

    feat: links de acesso facil

---
+++ src/components/search/SearchLinks.jsx
@@ -0,0 +1,34 @@
+import {Link} from "react-router-dom";
+import {Badge} from "react-bootstrap";
+
+const SearchLinks = () => {
+  return (
+    <div className={"py-3 d-flex align-items-center gap-1 overflow-x-scroll d-none"}>
+      {
+        [
+          ["Municipal", "bg-danger-subtle text-danger-emphasis"],
+          ["Metropolitano", "bg-primary-subtle text-primary-emphasis"],
+          ["Intermunicipal", "bg-primary-subtle text-primary-emphasis"],
+          ["Coletivo", "bg-danger-subtle text-danger-emphasis"],
+          ["Executivo", "bg-success-subtle text-success-emphasis"],
+        ].toSorted((a, b) =>
+          (a[1] ? 1 : 0) - (b[1] ? 1 : 0) || (a[1] ? a[1].localeCompare(b[1]) : a[0].localeCompare(b[0]))
+        ).map((item, index) => (
+          <Link key={index} to={"#"} className={"cursor-pointer"}>
+            <Badge key={index} className={"rounded-pill fw-normal d-inline-block inter fs-6 " + (item[1] ? item[1] : "bg-secondary-subtle text-secondary-emphasis")}>{item[0]}</Badge>
+          </Link>
+        ))
+      }
+      <span className={"text-body-tertiary fw-light"}>|</span>
+      {
+        [...[4999, 4997, 4993, 4992, 4991, 4988, 4987, 4986].map(i => [i])].map((item, index) => (
+          <Link key={index} to={"#"} className={"cursor-pointer"}>
+            <Badge className={"rounded-pill fw-normal d-inline-block inter fs-6 " + (item[1] ? item[1] : "bg-primary-subtle text-primary-emphasis")}>{item[0]}</Badge>
+          </Link>
+        ))
+      }
+    </div>
+  )
+}
+
+export default SearchLinks
