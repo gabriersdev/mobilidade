88b7bd3b479864bad7e1805a50142e6c56116d50
Gabriel Ribeiro <105074547+gabriersdev@users.noreply.github.com>
2025-04-30 09:24:41 -0300

    feat: links de pesquisa

--- src/components/search/SearchLinks.jsx
+++ src/components/search/SearchLinks.jsx
@@ -3,7 +3,7 @@ import {Badge} from "react-bootstrap";
 
 const SearchLinks = () => {
   return (
-    <div className={"py-3 d-flex align-items-center gap-1 overflow-x-scroll d-none"}>
+    <div className={"py-3 d-flex align-items-center gap-1 overflow-x-scroll"}>
       {
         [
           ["Municipal", "bg-danger-subtle text-danger-emphasis"],
@@ -14,15 +14,15 @@ const SearchLinks = () => {
         ].toSorted((a, b) =>
           (a[1] ? 1 : 0) - (b[1] ? 1 : 0) || (a[1] ? a[1].localeCompare(b[1]) : a[0].localeCompare(b[0]))
         ).map((item, index) => (
-          <Link key={index} to={"#"} className={"cursor-pointer"}>
+          <Link key={index} to={(typeof item[0]!== "string" ? item[0].toString().match(/\d/g) : item[0].match(/\d/g)) ? ((item[0].toString().match(/\d/g).join('').length === item[0].toString().length) ? "/search/?term=" + item[0] : "/search/?term=" + item[0].toLowerCase()) : "/search/?term=" + item[0].toLowerCase()} className={"cursor-pointer"}>
             <Badge key={index} className={"rounded-pill fw-normal d-inline-block inter fs-6 " + (item[1] ? item[1] : "bg-secondary-subtle text-secondary-emphasis")}>{item[0]}</Badge>
           </Link>
         ))
       }
       <span className={"text-body-tertiary fw-light"}>|</span>
       {
-        [...[4999, 4997, 4993, 4992, 4991, 4988, 4987, 4986].map(i => [i])].map((item, index) => (
-          <Link key={index} to={"#"} className={"cursor-pointer"}>
+        [...[4999, 4997, 4993, 4992, 4991, 4988, 4987, 4986, 4970, 4925, 4676, 4600].map(i => [i])].map((item, index) => (
+          <Link key={index} to={(item[0].toString().match(/\d/g)) ? ((item[0].toString().match(/\d/g).join('').length === item[0].toString().length) ? "/search/?term=" + item[0] : "/search/?term: " + item[0].toLowerCase()) : "/search/?term=tag: " + item[0].toLowerCase()} className={"cursor-pointer"}>
             <Badge className={"rounded-pill fw-normal d-inline-block inter fs-6 " + (item[1] ? item[1] : "bg-primary-subtle text-primary-emphasis")}>{item[0]}</Badge>
           </Link>
         ))
