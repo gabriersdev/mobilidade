8bae967f97e536a525c756f38841860b44a37def
Gabriel Ribeiro <105074547+gabriersdev@users.noreply.github.com>
2025-05-04 13:09:37 -0300

    feat: espaçamento excessivo corrigido

--- src/components/grid/Grid.jsx
+++ src/components/grid/Grid.jsx
@@ -2,7 +2,7 @@ import PropTypes from "prop-types";
 import "./grid.css";
 
 const Grid = ({classes, children, variant}) => {
-  return <div className={(classes ? `grid ${classes}` : "grid") + (variant ? ` ${variant}` : "")}>{children}</div>;
+  return <div className={(classes ? `grid ${classes !== 'main' ? classes : ""}` : "grid") + (variant ? ` ${variant}` : "")}>{children}</div>;
 }
 
 Grid.propTypes = {
