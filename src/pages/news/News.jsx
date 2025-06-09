75545b5c844c53c12ad94210db31c9cef6126fb5
Gabriel Ribeiro <105074547+gabriersdev@users.noreply.github.com>
2025-06-09 09:23:14 -0300

    del: removendo componente RenderNews e deps que ele usava

--- src/pages/news/News.jsx
+++ src/pages/news/News.jsx
@@ -1,40 +1,8 @@
-import {useParams} from "react-router-dom";
-import {useEffect, useState} from "react";
-import Alert from "../../components/alert/Alert.jsx";
-import FeedbackError from "../../components/feedbackError/FeedbackError.jsx";
+import {useEffect} from "react";
 import Title from "../../components/title/Title.jsx";
 import AnimatedComponents from "../../components/animatedComponent/AnimatedComponents.jsx";
 import NewsC from '../../components/news/News.jsx';
 import newsA from "../../assets/news.js";
-
-const RenderNews = () => {
-  const {id} = useParams();
-  const [error, setError] = useState(null);
-  const [loaded, setIsLoaded] = useState(true);
-  const [data, setData] = useState([]);
-  const [linesData, setLinesData] = useState([]);
-  
-  const checkIsValid = (id) => {
-    if (!id) return false
-    if (!id.length) return false
-    return id.match(/\d/g)
-  }
-  
-  useEffect(() => {
-    setIsLoaded(false);
-  }, [id])
-  
-  if (loaded) {
-    return <>Carregando...</>
-  } else if (error) {
-    console.error(error);
-    return <FeedbackError code={error.response ? error.response.status || 500 : 500} text={error.message} type={'card'}/>;
-  } else if (data.length === 0) {
-    return <Alert variant={'danger'} margin={"mt-0"}><span>Notícia não encontrada.</span></Alert>;
-  } else {
-    return <></>
-  }
-}
 
 const News = () => {
   useEffect(() => {
