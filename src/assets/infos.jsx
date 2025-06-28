bdf30cfa071bca0aa3c48b843188f4cc69c7f63c
Gabriel Ribeiro <105074547+gabriersdev@users.noreply.github.com>
2025-06-28 11:44:52 -0300

    feat: info adicionada

--- src/assets/infos.jsx
+++ src/assets/infos.jsx
@@ -1,9 +1,14 @@
 export default [
   {
     title: "Linhas afetadas pela redução do quadro de horários",
-    message: (<>Redução afeta as linhas 03, 04, 05 e 07 do transporte público municipal de Sabará-MG, operadas pela
-      Vinscol. Válido a partir de quinta-feira, 03<span className={"arial"}>/</span>04</>),
+    message: (<>Redução afeta as linhas 03, 04, 05 e 07 do transporte público municipal de Sabará-MG, operadas pela Vinscol. Válido a partir de quinta-feira, 03<span className={"arial"}>/</span>04</>),
     init: "2025-04-03T00:00:00-03:00",
     finish: "2025-04-13T00:00:00-03:00",
-  }
+  },
+  {
+    title: "Linhas afetadas pela construção do PSF Siderúrgica e reparação na ponte para o bairro Mangueiras",
+    message: (<>As obras de construção do posto de saúde próximo ao Terminal Rodoviário de Sabará e a reparação da ponte para o bairro Mangueiras afeta todas as linhas operadas pela Vinscol e as linhas 4970, 4987, 4988 e 4991. Atrasos podem ocorrer.</>),
+    init: "2025-06-30T00:00:00-03:00",
+    finish: "2025-07-10T00:00:00-03:00",
+  },
 ]
