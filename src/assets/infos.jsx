c3c98ecac6b62704746b6fd321455af5a360684a
Gabriel Ribeiro <105074547+gabriersdev@users.noreply.github.com>
2025-06-28 11:37:46 -0300

    refactor: organizacao das informacoes em assets e lint no Nav.jsx

---
+++ src/assets/infos.jsx
@@ -0,0 +1,9 @@
+export default [
+  {
+    title: "Linhas afetadas pela redução do quadro de horários",
+    message: (<>Redução afeta as linhas 03, 04, 05 e 07 do transporte público municipal de Sabará-MG, operadas pela
+      Vinscol. Válido a partir de quinta-feira, 03<span className={"arial"}>/</span>04</>),
+    init: "2025-04-03T00:00:00-03:00",
+    finish: "2025-04-13T00:00:00-03:00",
+  }
+]
