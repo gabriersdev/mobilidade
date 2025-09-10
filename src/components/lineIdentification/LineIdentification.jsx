adbb5d3a2349aa69e4a9d5887e68d91afa2e5411
Gabriel Ribeiro <105074547+gabriersdev@users.noreply.github.com>
2025-09-09 22:33:12 -0300

    feat: titulo h1

--- src/components/lineIdentification/LineIdentification.jsx
+++ src/components/lineIdentification/LineIdentification.jsx
@@ -54,7 +54,7 @@ const LineIdentification = ({line}) => {
   return (
     <div className="d-flex flex-column gap-3">
       <hgroup className="d-flex align-items-center gap-2 flex-wrap mb-0">
-        <h1 className={"d-none"}>Linha {line.line_number} {line.departure_location} para {line.destination_location}</h1>
+        <h1 className={"d-none"}>Linha {line.line_number} - {line.departure_location} para {line.destination_location}</h1>
         <Title type="h2" classX=" fs-2 d-inline text-body-emphasis m-0 p-0">Linha {line.line_number}</Title>
         <span className="text-body-secondary">|</span>
         <Title type="h2" classX=" fs-2 d-inline text-body-secondary m-0 p-0 lh-sm">{line.departure_location} -{">"} {line.destination_location}</Title>
