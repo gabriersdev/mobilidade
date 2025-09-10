9ebf8cc11a6ff66a531eb8ad8c1989688b4a42b9
Gabriel Ribeiro <105074547+gabriersdev@users.noreply.github.com>
2025-09-09 22:44:18 -0300

    raw: dados das noticias atualizados

--- src/assets/news.js
+++ src/assets/news.js
@@ -55,10 +55,10 @@ export default [
     publishDate: "2025-07-14T00:00:00.000-03:00",
   },
   {
-    title: "Feriado de 7 de setembro - mudanças de itinerário",
-    resume: "Durante o feriado de 7 de setembro (domingo) todas as linhas operadas pela Vinscol não passarão pelo centro Histórico.",
+    title: "Feriado de 07 de setembro - mudanças de itinerário",
+    resume: "Durante o feriado de 07 de setembro (domingo) todas as linhas operadas pela Vinscol não passarão pelo centro Histórico.",
     content: [
-      "No domingo, feriado de 7 de setembro todas as linhas operadas pela Vinscol não passarão pelo centro Histórico, em razão das comemorações e desfiles no Centro, seguindo viagem pela Av. Prefeito Victor Fantini e Av. Perimetral, sem entrar na Praça da Getúlio Vargas (Matriz), durante todo o dia, no sentido ida e volta.",
+      "No domingo, feriado de 07 de setembro todas as linhas operadas pela Vinscol não passarão pelo centro Histórico, em razão das comemorações e desfiles no Centro, seguindo viagem pela Av. Prefeito Victor Fantini e Av. Perimetral, sem entrar na Praça da Getúlio Vargas (Matriz), durante todo o dia, no sentido ida e volta.",
       "As linhas operadas pelo Consórcio Metropolitano <Link to={\"/company/4\"}>(Transporte Coletivo Metropolitano - MG)</Link> não são afetadas pelas comemorações de 7 de setembro.",
     ],
     img: "/news/neobus-44101-ob.jpg",
