afe8f686ad9634d57cb5fbf842770b84af916c85
Gabriel Ribeiro <105074547+gabriersdev@users.noreply.github.com>
2025-09-28 12:31:39 -0300

    feat: ocultando links

--- src/components/line/Line.jsx
+++ src/components/line/Line.jsx
@@ -137,8 +137,10 @@ const Line = ({id}) => {
               </div>
               <div className={"mt-3 d-flex gap-3 flex-wrap"}>
                 <Link to={`/history/departure-times/${id}`}>Histórico de horários</Link>
-                <Link to={`/history/#`}>Histórico de tarifas</Link>
-                <Link to={`/history/#`}>Histórico de pontos de paradas</Link>
+                <div className={"d-none"}>
+                  <Link to={`/history/#`}>Histórico de tarifas</Link>
+                  <Link to={`/history/#`}>Histórico de pontos de paradas</Link>
+                </div>
               </div>
               <div className={"mt-5 d-flex flex-column gap-3"}>
                 <GuideBanner/>
