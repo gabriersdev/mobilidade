0c70f1702c13e0ab46df8cd7a092af5231b2c879
Gabriel Ribeiro <105074547+gabriersdev@users.noreply.github.com>
2025-10-01 11:47:45 -0300

    feat: informacoes da cidade

--- src/components/weather/CityInfo.jsx
+++ src/components/weather/CityInfo.jsx
@@ -11,7 +11,7 @@ const CityInfo = ({weatherData}) => {
   const translatedCondition = translateWeather(current.condition?.text);
   
   return (
-    <p className={"m-0 p-0 d-flex flex-wrap align-items-center lh-base gap-1"}>
+    <p className={"m-0 p-0 d-flex flex-wrap align-items-center lh-base gap-1 text-balance"}>
       <span>Agora em Sabará fazem {current?.["temp_c"] ?? "0"}º graus.</span>
       <span>A umidade relativa do ar é de {current?.["humidity"] ?? "0"}%. Beba água.</span>
       <span>O tempo é {translatedCondition ?? "bom"}</span>
