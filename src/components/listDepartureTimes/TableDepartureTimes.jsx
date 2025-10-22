d10d8436f94b42f3317ada931ed393432333495b
Gabriel Ribeiro <105074547+gabriersdev@users.noreply.github.com>
2025-10-22 13:28:50 -0300

    feat: atualizacao das informacoes do componente

--- src/components/listDepartureTimes/TableDepartureTimes.jsx
+++ src/components/listDepartureTimes/TableDepartureTimes.jsx
@@ -1,4 +1,4 @@
-import {useContext, useState, useEffect} from "react";
+import {useContext, useState, useEffect, useCallback} from "react";
 import PropTypes from "prop-types";
 import {Badge, Table as BootstrapTable} from "react-bootstrap";
 import data from "../../data";
@@ -8,64 +8,81 @@ import moment from "moment";
 
 const TableRow = ({row, directionName, direction, dayName, tableIndex, type}) => {
   const {defaultEventKey, handlePointClick} = useContext(TimeContext);
+  const [content, setContent] = useState(<></>);
   const bootstrapBGColors = data.bootstrap.bg.colors;
   
-  return (
-    <tr>
-      {row.map((item, timeOrder) => (
-        <td
-          key={`${item.departureTime}-${timeOrder}`}
-          onClick={(e) =>
-            handlePointClick(e, {
-              time: item.departureTime,
-              timeOrder,
-              timeLength: row.length,
-              directionName,
-              direction,
-              dayName,
-              observations: item.observations?.map((observation) => ({
-                abrev: observation.abrev,
-                label: observation.label,
-              })),
-            })
-          }
-          style={{cursor: "pointer"}}
-        >
-          <div className="d-flex align-items-center">
-            {item.departureTime}
-            
-            
-            {(type !== "history") && tableIndex.toString() === defaultEventKey?.[0] && (
-              <>
-                {moment(`2020-01-01T${moment().format("HH:mm")}:00`).diff(moment(`2020-01-01T${item.departureTime}:00`), "seconds") < -(15 * 60) ? (
-                  <div className="text-secondary d-inline-block">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-fast-forward-circle-fill pb-1" viewBox="0 0 16 16">
-                      <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16M4.79 5.093 8 7.386V5.5a.5.5 0 0 1 .79-.407l3.5 2.5a.5.5 0 0 1 0 .814l-3.5 2.5A.5.5 0 0 1 8 10.5V8.614l-3.21 2.293A.5.5 0 0 1 4 10.5v-5a.5.5 0 0 1 .79-.407"/>
-                    </svg>
-                  </div>
-                ) : moment(`2020-01-01T${moment().format("HH:mm")}:00`).diff(moment(`2020-01-01T${item.departureTime}:00`), "seconds") <= 0 && (
-                  <div className="text-danger d-inline-flex align-items-center">
-                    <span className={"px-1 text-sml"}>Prox.</span>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-fast-forward-circle-fill" viewBox="0 0 16 16">
-                      <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16M4.79 5.093 8 7.386V5.5a.5.5 0 0 1 .79-.407l3.5 2.5a.5.5 0 0 1 0 .814l-3.5 2.5A.5.5 0 0 1 8 10.5V8.614l-3.21 2.293A.5.5 0 0 1 4 10.5v-5a.5.5 0 0 1 .79-.407"/>
-                    </svg>
-                  </div>
+  useEffect(() => {
+    const render = () => {
+      return (
+        <tr>
+          {row.map((item, timeOrder) => (
+            <td
+              key={`${item.departureTime}-${timeOrder}`}
+              onClick={(e) =>
+                handlePointClick(e, {
+                  time: item.departureTime,
+                  timeOrder,
+                  timeLength: row.length,
+                  directionName,
+                  direction,
+                  dayName,
+                  observations: item.observations?.map((observation) => ({
+                    abrev: observation.abrev,
+                    label: observation.label,
+                  })),
+                })
+              }
+              style={{cursor: "pointer"}}
+            >
+              <div className="d-flex align-items-center">
+                {item.departureTime}
+                
+                
+                {(type !== "history") && tableIndex.toString() === defaultEventKey?.[0] && (
+                  <>
+                    {moment(`2020-01-01T${moment().format("HH:mm")}:00`).diff(moment(`2020-01-01T${item.departureTime}:00`), "seconds") < -(15 * 60) ? (
+                      <div className="text-secondary d-inline-block">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-fast-forward-circle-fill pb-1" viewBox="0 0 16 16">
+                          <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16M4.79 5.093 8 7.386V5.5a.5.5 0 0 1 .79-.407l3.5 2.5a.5.5 0 0 1 0 .814l-3.5 2.5A.5.5 0 0 1 8 10.5V8.614l-3.21 2.293A.5.5 0 0 1 4 10.5v-5a.5.5 0 0 1 .79-.407"/>
+                        </svg>
+                      </div>
+                    ) : moment(`2020-01-01T${moment().format("HH:mm")}:00`).diff(moment(`2020-01-01T${item.departureTime}:00`), "seconds") <= 0 && (
+                      <div className="text-danger d-inline-flex align-items-center">
+                        <span className={"px-1 text-sml"}>Prox.</span>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-fast-forward-circle-fill" viewBox="0 0 16 16">
+                          <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16M4.79 5.093 8 7.386V5.5a.5.5 0 0 1 .79-.407l3.5 2.5a.5.5 0 0 1 0 .814l-3.5 2.5A.5.5 0 0 1 8 10.5V8.614l-3.21 2.293A.5.5 0 0 1 4 10.5v-5a.5.5 0 0 1 .79-.407"/>
+                        </svg>
+                      </div>
+                    )}
+                  </>
                 )}
-              </>
-            )}
-            
-            {
-              (item.observations?.map((observation, i) => (
-                <Badge key={i} className="ms-1" bg={bootstrapBGColors[observation.index]}>
-                  {observation.abrev}
-                </Badge>
-              )))
-            }
-          </div>
-        </td>
-      ))}
-    </tr>
-  );
+                
+                {
+                  (item.observations?.map((observation, i) => (
+                    <Badge key={i} className="ms-1" bg={bootstrapBGColors[observation.index]}>
+                      {observation.abrev}
+                    </Badge>
+                  )))
+                }
+              </div>
+            </td>
+          ))}
+        </tr>
+      )
+    };
+    
+    let tmt;
+    
+    tmt = setInterval(() => {
+      setContent(render());
+    }, 1000);
+    
+    return () => {
+      clearInterval(tmt);
+    }
+  }, [bootstrapBGColors, dayName, defaultEventKey, direction, directionName, handlePointClick, row, tableIndex, type]);
+  
+  return (<>{content}</>);
 };
 
 const TableDepartureTimes = ({content, tableIndex}) => {
