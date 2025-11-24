487dac5dbb181cbc34b181845e74f075eed14060
Gabriel Ribeiro <105074547+gabriersdev@users.noreply.github.com>
2025-11-24 01:53:50 -0300

    feat: adicao de link no Footer.jsx para manifesto

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -87,9 +87,10 @@ const Footer = () => {
         <Container className="d-flex flex-column gap-2rem">
           <p className="mb-0 text-body-secondary">&copy; {new Date().getFullYear() || '2024'} Mobilidade</p>
           <ul className="d-flex flex-column g-3 m-0 p-0 footer-link-list">
-            <Link to={"./development#topo"} className="footer-link-list-item">Desenvolvimento</Link>
-            <Link to="./terms-of-service#topo" className="footer-link-list-item">Termos de serviços</Link>
-            <Link to="./privacy#topo" className="footer-link-list-item">Privacidade</Link>
+            <Link to={"/development#topo"} className="footer-link-list-item">Desenvolvimento</Link>
+            <Link to="/terms-of-service#topo" className="footer-link-list-item">Termos de serviços</Link>
+            <Link to="/privacy#topo" className="footer-link-list-item">Privacidade</Link>
+            <Link to="/manifest#topo" className="footer-link-list-item">Manifesto</Link>
             <Link to="https://github.com/gabriersdev/mobilidade/" className="footer-link-list-item">Repositório no Github</Link>
             <Link to="https://github.com/gabriersdev/mobilidade/issues/new" className="footer-link-list-item">Reportar erro</Link>
           </ul>
