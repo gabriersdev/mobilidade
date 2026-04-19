4399b1b85904845524855e153a07316431a9e189
Gabriel Ribeiro <105074547+gabriersdev@users.noreply.github.com>
2026-04-19 08:08:16 -0300

    refactor: bus-time-display.jsx

--- src/components/suggest-label/bus-times-display.jsx
+++ src/components/suggest-label/bus-times-display.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import {Link} from "react-router-dom";
+import {Link} from 'react-router-dom';
+import BusTimePlaceholder from './bus-time-placeholder.jsx';
 
-// TODO - aplicar aqui, tal qual as funções de @live-infos, a verificação se está carregando, se já tem resultado e tal
-const BusTimesDisplay = ({busTimes}) => (
-  <div className="d-flex gap-1 align-items-center flex-wrap text-sml">
-    {busTimes.length > 0 ? (
-      busTimes.map((busTime, index) => (
+const BusTimesDisplay = ({busTimes, loading}) => {
+  if (loading) {
+    return <BusTimePlaceholder/>;
+  }
+  
+  if (busTimes.length === 0) {
+    return <span className="text-muted">Nenhum ônibus por perto</span>;
+  }
+  
+  return (
+    <div className="d-flex gap-1 align-items-center flex-wrap text-sml">
+      {busTimes.map((busTime, index) => (
         <React.Fragment key={busTime.id}>
           <div>
             <Link className="text-body text-decoration-none" to={busTime.link}>
@@ -17,24 +25,14 @@ const BusTimesDisplay = ({busTimes}) => (
             <span className="fs-inherit text-body-tertiary">|</span>
           )}
         </React.Fragment>
-      ))
-    ) : (
-      <span className="text-muted">Nenhum ônibus por perto</span>
-    )}
-    
-    {/*TODO - usar placeholder para mostrar carregamento enquando não tiver o retorno CONFIRMADO que não tem NADA chegando no ponto*/}
-    {
-      Array.from({length: 3}, (i) => i + 1).map((_, i) => (
-        <div className={"d-flex flex-column placeholder-glow gap-1 mb-1"} key={i}>
-          <div className={'placeholder fs-4'} style={{minWidth: 200}}></div>
-        </div>
-      ))
-    }
-  </div>
-);
+      ))}
+    </div>
+  );
+};
 
 BusTimesDisplay.propTypes = {
   busTimes: PropTypes.array.isRequired,
+  loading: PropTypes.bool.isRequired,
 };
 
 export default BusTimesDisplay;
