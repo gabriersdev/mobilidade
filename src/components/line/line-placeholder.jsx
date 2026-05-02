8ca4fc6c14dfb6a58fbc788831e1dcbd944f1e90
Gabriel Ribeiro <105074547+gabriersdev@users.noreply.github.com>
2026-05-02 16:04:57 -0300

    refactor: line.jsx

---
+++ src/components/line/line-placeholder.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import { Placeholder } from 'react-bootstrap';
+import AnimatedComponents from '@/components/ui/animated-component/animated-components.jsx';
+
+const LinePlaceholder = () => (
+  <AnimatedComponents>
+    <section className="d-flex flex-column gap-4 gap-sm-5">
+      <div className="d-flex align-items-start justify-content-between gap-3">
+        <Placeholder animation="glow" className="w-50">
+          <Placeholder className="rounded w-100 py-4" />
+        </Placeholder>
+        <Placeholder animation="glow" className="w-25">
+          <Placeholder className="rounded w-100 py-5" />
+        </Placeholder>
+      </div>
+      <div className="d-flex flex-column gap-2 mt-3 mt-md-5">
+        {[...Array(5)].map((_, index) => (
+          <div key={index}>
+            <Placeholder animation="glow">
+              <Placeholder className={`rounded-1 w-${[15, 10, 25, 10, 25][index % 5]} py-3`} />
+            </Placeholder>
+            <span> </span>
+          </div>
+        ))}
+      </div>
+      {Array.from({ length: 5 }).map((_, index) => (
+        <div className="d-flex flex-column mt-3 mt-md-5" key={index}>
+          <div className="d-flex align-items-center justify-content-between gap-3">
+            <Placeholder animation="glow" className="w-25">
+              <Placeholder className="rounded-1 w-100 py-4" />
+            </Placeholder>
+            <Placeholder animation="glow" className="w-10">
+              <Placeholder className="rounded-1 w-100 py-3" />
+            </Placeholder>
+          </div>
+          <Placeholder animation="glow" className="w-100 mt-4">
+            <Placeholder className="rounded-1 w-100 py-5">
+              <div className="py-5"></div>
+              <div className="py-5"></div>
+              <div className="py-5"></div>
+            </Placeholder>
+          </Placeholder>
+        </div>
+      ))}
+    </section>
+  </AnimatedComponents>
+);
+
+export default LinePlaceholder;
--- src/components/line/line.jsx
+++ src/components/line/line.jsx
@@ -1,141 +1,27 @@
 import {useRef} from "react";
 import PropTypes from "prop-types";
-import {Placeholder} from "react-bootstrap";
 
-// TODO - refatorar componente
-import {LineContext} from "@/components/line/line-context.jsx";
 import {useLineData} from "@/components/line/use-line-data.js";
 import {usePageEffects} from "@/components/line/use-page-effects.js";
-import AnimatedComponents from "@/components/ui/animated-component/animated-component.jsx";
+import AnimatedComponents from "@/components/ui/animated-component/animated-components.jsx";
 import FeedbackError from "@/components/ui/feedback-error/feedback-error.jsx";
 import Alert from "@/components/ui/alert/alert.jsx";
-import LineHeader from "@/components/line/line-header.jsx";
-import DepartureTimesSection from "@/components/line/departure-times-section.jsx";
-import DeparturePointsSection from "@/components/line/departure-points-section.jsx";
-import RenderLiveMap from "@/components/line/render-live-map.jsx";
-import RechargePointsSection from "@/components/line/recharge-points-section.jsx";
-import SimilarLinesSection from "@/components/line/similar-lines-section.jsx";
-import AboutSection from "@/components/line/about-section.jsx";
+import LinePlaceholder from "@/components/line/line-placeholder.jsx";
+import LineDetails from "@/components/line/line-details.jsx";
 
 const Line = ({id}) => {
   const {data, error, isLoaded} = useLineData(id);
-  const paradasSection = useRef(<></>);
+  const paradasSection = useRef(null);
   usePageEffects(data, paradasSection);
   
   if (isLoaded) {
-    // if (true) {
-    return (
-      // Placeholder de carregamento de dados das linhas
-      <AnimatedComponents>
-        {/*Toda a seção da página*/}
-        <section className={"d-flex flex-column gap-4 gap-sm-5"}>
-          {/*Título de identificação da linha e logo da operadora*/}
-          <div className={"d-flex align-items-start justify-content-between gap-3"}>
-            <Placeholder animation="glow" className={"w-50"}>
-              <Placeholder className={"rounded w-100 py-4"}/>
-            </Placeholder>
-            
-            <Placeholder animation="glow" className={"w-25"}>
-              <Placeholder className={"rounded w-100 py-5"}/>
-            </Placeholder>
-          </div>
-          
-          {/*Parte de informações básicas da linha*/}
-          <div className={"d-flex flex-column gap-2 mt-3 mt-md-5"}>
-            <div>
-              <Placeholder animation="glow">
-                <Placeholder className={"rounded-1 w-15 py-3"}/>
-              </Placeholder>
-              
-              <span>{" "}</span>
-              
-              <Placeholder animation="glow">
-                <Placeholder className={"rounded-1 w-10 py-3"}/>
-              </Placeholder>
-              
-              <span>{" "}</span>
-              
-              <Placeholder animation="glow">
-                <Placeholder className={"rounded-1 w-25 py-3"}/>
-              </Placeholder>
-            </div>
-            
-            <div>
-              <Placeholder animation="glow">
-                <Placeholder className={"rounded-1 w-10 py-3"}/>
-              </Placeholder>
-              
-              <span>{" "}</span>
-              
-              <Placeholder animation="glow">
-                <Placeholder className={"rounded-1 w-25 py-3"}/>
-              </Placeholder>
-            </div>
-            
-            <div>
-              <Placeholder animation="glow">
-                <Placeholder className={"rounded-1 w-15 py-3"}/>
-              </Placeholder>
-            </div>
-            
-            <div>
-              <Placeholder animation="glow">
-                <Placeholder className={"rounded-1 w-25 py-3"}/>
-              </Placeholder>
-            </div>
-            
-            <div className={"mt-3"}>
-              <Placeholder animation="glow">
-                <Placeholder className={"rounded-1 w-10 py-3"}/>
-              </Placeholder>
-              
-              <span>{" "}</span>
-              
-              <Placeholder animation="glow">
-                <Placeholder className={"rounded-1 w-15 py-3"}/>
-              </Placeholder>
-              
-              <span>{" "}</span>
-              
-              <Placeholder animation="glow">
-                <Placeholder className={"rounded-1 w-10 py-3"}/>
-              </Placeholder>
-            </div>
-          </div>
-          
-          {/*Correponde as seções de Horários de Partida, Pontos de Paradas e Recargas e banner's*/}
-          {
-            Array.from({length: 5}, i => i + 1).map((_, index) => (
-              <div className={"d-flex flex-column mt-3 mt-md-5"} key={index}>
-                <div className={"d-flex align-items-center justify-content-between gap-3"}>
-                  <Placeholder animation="glow" className={"w-25"}>
-                    <Placeholder className={"rounded-1 w-100 py-4"}/>
-                  </Placeholder>
-                  
-                  <Placeholder animation="glow" className={"w-10"}>
-                    <Placeholder className={"rounded-1 w-100 py-3"}/>
-                  </Placeholder>
-                </div>
-                
-                <Placeholder animation="glow" className={"w-100 mt-4"} key={index}>
-                  <Placeholder className={"rounded-1 w-100 py-5"}>
-                    <div className={"py-5"}></div>
-                    <div className={"py-5"}></div>
-                    <div className={"py-5"}></div>
-                  </Placeholder>
-                </Placeholder>
-              </div>
-            ))
-          }
-        </section>
-      </AnimatedComponents>
-    );
+    return <LinePlaceholder/>;
   }
   
   if (error) {
     return (
       <AnimatedComponents>
-        <FeedbackError code={error.response ? error.response.status || 500 : 500} text={error.message} type={'card'}/>
+        <FeedbackError code={error.response?.status || 500} text={error.message} type={'card'}/>
       </AnimatedComponents>
     );
   }
@@ -153,31 +39,11 @@ const Line = ({id}) => {
     );
   }
   
-  const line = data[0];
-  
-  return (
-    <AnimatedComponents>
-      <div className="d-flex flex-column" style={{gap: '3rem'}}>
-        <LineContext line={line}>
-          <AnimatedComponents>
-            <div className="d-flex flex-column" style={{gap: '3rem'}}>
-              <LineHeader line={line}/>
-              <DepartureTimesSection line={line}/>
-              <DeparturePointsSection line={line} ref={paradasSection}/>
-              <RenderLiveMap data={data}/>
-              <RechargePointsSection line={line}/>
-              <SimilarLinesSection/>
-              <AboutSection line={line}/>
-            </div>
-          </AnimatedComponents>
-        </LineContext>
-      </div>
-    </AnimatedComponents>
-  );
+  return <LineDetails line={data[0]} paradasSection={paradasSection} data={data}/>;
 };
 
 Line.propTypes = {
-  id: PropTypes.string.isRequired
+  id: PropTypes.string.isRequired,
 };
 
 export default Line;
