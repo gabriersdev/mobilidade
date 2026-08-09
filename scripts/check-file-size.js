6dea47d2a2d2c613e1dae4488f5dbc7fcad55440
Gabriel Ribeiro <105074547+gabriersdev@users.noreply.github.com>
2026-08-08 21:45:27 -0300

    build: update version

--- scripts/check-file-size.js
+++ scripts/check-file-size.js
@@ -1,7 +1,7 @@
 import {readFile} from 'fs/promises';
 import path from 'path';
 import {fileURLToPath} from 'url';
-import {getFiles} from '@/lib/util.js';
+import {getFiles} from '../src/lib/util.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
